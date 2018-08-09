53e88dc75228801d8162e01ebfa085271d13c8c4
DaveBrun94 <david.brunner@rwth-aachen.de>
2018-08-09 18:48:26 +0200

    Did stuff

--- compile_command.sh
+++ compile_command.sh
@@ -1,7 +1,9 @@
 pdflatex --jobname LFV_thesis main
+pdflatex --jobname LFV_thesis main
 bibtex LFV_thesis
+makeglossaries LFV_thesis
 pdflatex --jobname LFV_thesis main
 pdflatex --jobname LFV_thesis main
 GLOBIGNORE='LFV_thesis.pdf'
-rm LFV_thesis.* *~ */*~
+rm LFV_thesis.*
 unset GLOBIGNORE
